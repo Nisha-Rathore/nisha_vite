4a80713f824428041806ffd242f93b0b1a2aaeb9
Nisha-Rathore <rathorenisha397@gmail.com>
2025-07-04 12:57:27 +0530

    update

--- src/component/Main2.jsx
+++ src/component/Main2.jsx
@@ -11,16 +11,71 @@ export default function Main2() {
                 </p>
               </div>
            </div>
-           <div className="card" style={{width:"700px",border:"1px 1px solid gray",borderRadius:"20px",boxShadow:"3px 1px 3px 2px gray"}}>
+          <div style={{display:"flex"}}>
+             <div className="card" style={{width:"650px",border:"1px 1px solid gray",borderRadius:"20px",boxShadow:"3px 1px 3px 2px gray",textAlign:"left"}}>
             <div></div>
-            <div style={{color:"blue"}}>Our Mission</div>
-            <p style={{color:"black"}}>To empower businesses with innovative technology solutions that drive growth, efficiency, and success. We are committed to delivering excellence in every project, fostering long-term partnerships, and creating sustainable value for our clients.</p>
-
-
-            
-               
+            <div style={{color:"blue",fontSize:"27px",fontWeight:"bold"}}>Our Mission</div>
+            <p style={{color:"black"}}>To empower businesses with innovative technology solutions that drive growth, efficiency, and success. We are committed to delivering excellence in every project, fostering long-term partnerships, and creating sustainable value for our clients.</p>    
            </div>
 
+            <div className="card" style={{width:"650px",border:"1px 1px solid gray",borderRadius:"20px",boxShadow:"3px 1px 3px 2px gray",textAlign:"left"}}>
+            <div></div>
+            <div style={{color:"blue",fontSize:"27px",fontWeight:"bold"}}>Our Vision</div>
+            <p style={{color:"black"}}>To be the global leader in technology innovation, setting new standards in digital transformation and becoming the trusted partner of choice for businesses seeking to thrive in the digital age.</p>    
+           </div>
+          </div>
+        <div style={{display:"flex"}}>
+            <div className="smallCard" style={{padding:"30px 0px"}} >
+                <div style={{width:"290px",height:"180px",border:"1px 1px solid gray",borderRadius:"20px",boxShadow:"3px 1px 3px 2px gray",textAlign:"left",padding:"0px 25px"}}>
+                    <p style={{color:"red",fontSize:"35px",fontWeight:"bolder",paddingTop:"20px"}}>10+</p>
+                    <p style={{color:"black",paddingTop:"0px"}}>Years Experience</p>
+                </div>
+            </div>
+            <div className="smallCard" style={{padding:"30px 0px"}} >
+                <div style={{width:"290px",height:"180px",border:"1px 1px solid gray",borderRadius:"20px",boxShadow:"3px 1px 3px 2px gray",textAlign:"left",padding:"0px 25px"}}>
+                    <p style={{color:"skyblue",fontSize:"35px",fontWeight:"bolder",paddingTop:"20px"}}>500+</p>
+                    <p style={{color:"black",paddingTop:"0px"}}>Projects Completed</p>
+                </div>
+            </div>
+            <div className="smallCard" style={{padding:"30px 0px"}} >
+                <div style={{width:"290px",height:"180px",border:"1px 1px solid gray",borderRadius:"20px",boxShadow:"3px 1px 3px 2px gray",textAlign:"left",padding:"0px 25px"}}>
+                    <p style={{color:"pink",fontSize:"35px",fontWeight:"bolder",paddingTop:"20px"}}>50+</p>
+                    <p style={{color:"black",paddingTop:"0px"}}>Team Members</p>
+                </div>
+            </div>
+            <div className="smallCard" style={{padding:"30px 0px"}} >
+                <div style={{width:"290px",height:"180px",border:"1px 1px solid gray",borderRadius:"20px",boxShadow:"3px 1px 3px 2px gray",textAlign:"left",padding:"0px 25px"}}>
+                    <p style={{color:"orange",fontSize:"35px",fontWeight:"bolder",paddingTop:"20px"}}>20+</p>
+                    <p style={{color:"black",paddingTop:"0px"}}>Countries Served</p>
+                </div>
+            </div>
+        </div>
+         <div style={{display:"flex"}}>
+            <div className="smallCard" style={{padding:"30px 0px"}} >
+                <div style={{width:"290px",height:"180px",border:"1px 1px solid gray",borderRadius:"20px",boxShadow:"3px 1px 3px 2px gray",textAlign:"left",padding:"0px 25px"}}>
+                    <p style={{color:"red",fontSize:"35px",fontWeight:"bolder",paddingTop:"20px"}}>Innovation</p>
+                    <p style={{color:"black",paddingTop:"0px"}}>Pushing boundaries with cutting-edge solutions</p>
+                </div>
+            </div>
+            <div className="smallCard" style={{padding:"30px 0px"}} >
+                <div style={{width:"290px",height:"180px",border:"1px 1px solid gray",borderRadius:"20px",boxShadow:"3px 1px 3px 2px gray",textAlign:"left",padding:"0px 25px"}}>
+                    <p style={{color:"skyblue",fontSize:"35px",fontWeight:"bolder",paddingTop:"20px"}}>Excellence</p>
+                    <p style={{color:"black",paddingTop:"0px"}}>Delivering quality in everything we do</p>
+                </div>
+            </div>
+            <div className="smallCard" style={{padding:"30px 0px"}} >
+                <div style={{width:"290px",height:"180px",border:"1px 1px solid gray",borderRadius:"20px",boxShadow:"3px 1px 3px 2px gray",textAlign:"left",padding:"0px 25px"}}>
+                    <p style={{color:"pink",fontSize:"35px",fontWeight:"bolder",paddingTop:"20px"}}>Integrity</p>
+                    <p style={{color:"black",paddingTop:"0px"}}>Building trust through honest practices</p>
+                </div>
+            </div>
+            <div className="smallCard" style={{padding:"30px 0px"}} >
+                <div style={{width:"290px",height:"180px",border:"1px 1px solid gray",borderRadius:"20px",boxShadow:"3px 1px 3px 2px gray",textAlign:"left",padding:"0px 25px"}}>
+                    <p style={{color:"orange",fontSize:"35px",fontWeight:"bolder",paddingTop:"20px"}}>Collaboration</p>
+                    <p style={{color:"black",paddingTop:"0px"}}>Working together to achieve greatness</p>
+                </div>
+            </div>
+        </div>
         </div>
         </>
     )
